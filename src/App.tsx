4aa1e824f7ca6173140f4d80ea7a6ee88262c071
Волков Алексей <wolf.alex.1997@mail.ru>
2022-09-15 21:19:25 +0300

    логика по переключению темы вынесена в отдельный хук useTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useContext, useState } from "react";
 // import Counter from "./components/Counter";
-import './index.scss'
+import './styles/index.scss'
 import { Link, Route, Routes } from "react-router-dom";
 // import AboutPage from "./pages/AboutPage/AboutPage";
 // import MainPage from "./pages/MainPage/MainPage";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Theme, ThemeContext } from "./theme/ThemeContext";
+import {useTheme} from "./theme/useTheme";
+
+
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Переключить тему</button>
             <Link to="/">Главная</Link>
             <Link to="/about">О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
