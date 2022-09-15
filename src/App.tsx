b072be4fcfda3c8443a875ed482c73033d9b085a
Волков Алексей <wolf.alex.1997@mail.ru>
2022-09-15 22:21:42 +0300

    добавлена функция classNames для более удобнойработы с классами

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,16 @@ import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { Theme, ThemeContext } from "./theme/ThemeContext";
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
 
 
 
 const App = () => {
-    const {theme, toggleTheme} = useTheme()
+    const {theme, toggleTheme} = useTheme();
+    const bool = true;
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>Переключить тему</button>
             <Link to="/">Главная</Link>
             <Link to="/about">О сайте</Link>
