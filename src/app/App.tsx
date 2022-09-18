67d4e566e9a65d0cbd5fc62393702b0c995a1ba4
Волков Алексей <wolf.alex.1997@mail.ru>
2022-09-18 18:43:35 +0300

    Создана компонента AppRouter и routerConfig

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
-import React, { Suspense } from "react";
+import React from "react";
 import './styles/index.scss'
-import { Link, Route, Routes } from "react-router-dom";
-
+import { Link } from "react-router-dom";
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
+
 
 
 
@@ -19,13 +18,8 @@ const App = () => {
             <button onClick={toggleTheme}>Переключить тему</button>
             <Link to="/">Главная</Link>
             <Link to="/about">О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage />} />
-                    <Route path={'/'} element={<MainPage />} />
-                </Routes>
-            </Suspense>
-            {/* <Counter /> */}
+            <AppRouter />
+
         </div>
     )
 }
