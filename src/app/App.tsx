0bde0e6723a7fb76fd3184680dcaf84c33a18953
Волков Алексей <wolf.alex.1997@mail.ru>
2022-09-18 17:43:46 +0300

    Отрефакторен код под архитектуру, добавлен хук useTheme

---
+++ src/app/App.tsx
@@ -0,0 +1,32 @@
+import React, { Suspense } from "react";
+import './styles/index.scss'
+import { Link, Route, Routes } from "react-router-dom";
+
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
+
+
+
+
+
+const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <button onClick={toggleTheme}>Переключить тему</button>
+            <Link to="/">Главная</Link>
+            <Link to="/about">О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPage />} />
+                    <Route path={'/'} element={<MainPage />} />
+                </Routes>
+            </Suspense>
+            {/* <Counter /> */}
+        </div>
+    )
+}
+export default App;
