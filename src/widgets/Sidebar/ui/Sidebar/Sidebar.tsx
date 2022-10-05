5541ac88214c1545377435e1c795edb092d47154
Волков Алексей <wolf.alex.1997@mail.ru>
2022-10-05 05:12:10 +0300

    Переработан сайдбар, дбавлены иконки

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -4,6 +4,9 @@ import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import AboutIcon from 'shared/assets/icons/about.svg';
+import MainIcon from 'shared/assets/icons/home-icon.svg';
+import { useTranslation } from 'react-i18next';
 import cls from './Sidebar.module.scss';
 import { Button, ButtonSize, ButtonTheme } from '../../../Button/Button';
 
@@ -13,6 +16,7 @@ interface SidebarProps {
 
 export const Sidebar = ({ className }: SidebarProps) => {
   const [collapsed, setCollapsed] = useState(false);
+  const { t } = useTranslation();
   const onToggle = () => {
     setCollapsed((prev) => !prev);
   };
@@ -35,16 +39,20 @@ export const Sidebar = ({ className }: SidebarProps) => {
         <AppLink
           theme={AppLinkTheme.SECONDARY}
           to={RoutePath.main}
-          className={cls.link}
+          className={cls.item}
         >
-          Главная
+          <MainIcon className={cls.icon} />
+          <span className={cls.link}>
+            {t('Главная')}
+          </span>
         </AppLink>
         <AppLink
           theme={AppLinkTheme.SECONDARY}
           to={RoutePath.about}
-          className={cls.link}
+          className={cls.item}
         >
-          О сайте
+          <AboutIcon className={cls.icon} />
+          <span className={cls.link}>{t('О сайте')}</span>
         </AppLink>
       </div>
       <div className={cls.switchers}>
