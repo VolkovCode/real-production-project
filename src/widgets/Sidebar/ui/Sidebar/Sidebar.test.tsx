0e366466bebfce18526bd2476b7421a0f862e919
Волков Алексей <wolf.alex.1997@mail.ru>
2022-10-05 05:34:17 +0300

    fix tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,15 +1,16 @@
 import { fireEvent, screen } from '@testing-library/react';
 import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 import { Sidebar } from './Sidebar';
 
 describe('Sidebar', () => {
   test('', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('Sidebar')).toBeInTheDocument();
   });
 
   test('test toggle', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     const toggleBtn = screen.getByTestId('sidebar-toggle');
     expect(screen.getByTestId('Sidebar')).toBeInTheDocument();
     fireEvent.click(toggleBtn);
@@ -17,7 +18,7 @@ describe('Sidebar', () => {
   });
 
   test('', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('Sidebar')).toBeInTheDocument();
   });
 });
