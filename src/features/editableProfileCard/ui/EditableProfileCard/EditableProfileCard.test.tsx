90f902bbf8575f5aa3cb83aefad66de67d39c986
Волков Алексей <wolf.alex.1997@mail.ru>
2023-01-20 00:52:10 +0300

    Добавлены тесты на EditableProfileCard

--- src/features/editableProfileCard/ui/EditableProfileCard/EditableProfileCard.test.tsx
+++ src/features/editableProfileCard/ui/EditableProfileCard/EditableProfileCard.test.tsx
@@ -4,6 +4,7 @@ import { Profile } from 'entities/Profile';
 import { Currency } from 'entities/Currency';
 import { Country } from 'entities/Country';
 import userEvent from '@testing-library/user-event';
+import { $api } from 'shared/api/api';
 import { profileReducer } from '../../model/slice/profileSlice';
 import { EditableProfileCard } from './EditableProfileCard';
 
@@ -44,9 +45,46 @@ describe('features/EditableProfileCard', () => {
         expect(screen.getByTestId('EditableProfileCardHeader.CancelButton')).toBeInTheDocument();
     });
 
-    test('Режим рид онли должен переключиться', async () => {
+    test('При отмене значения должны обнулиться', async () => {
         componentRender(<EditableProfileCard id="1" />, options);
         await userEvent.click(screen.getByTestId('EditableProfileCardHeader.EditButton'));
-        expect(screen.getByTestId('EditableProfileCardHeader.CancelButton')).toBeInTheDocument();
+        await userEvent.clear(screen.getByTestId('ProfileCard.firstname'));
+        await userEvent.clear(screen.getByTestId('ProfileCard.lastname'));
+
+        await userEvent.type(screen.getByTestId('ProfileCard.firstname'), 'user');
+        await userEvent.type(screen.getByTestId('ProfileCard.lastname'), 'user');
+
+        expect(screen.getByTestId('ProfileCard.firstname')).toHaveValue('user');
+        expect(screen.getByTestId('ProfileCard.lastname')).toHaveValue('user');
+
+        await userEvent.click(screen.getByTestId('EditableProfileCardHeader.CancelButton'));
+
+        expect(screen.getByTestId('ProfileCard.firstname')).toHaveValue('admin');
+        expect(screen.getByTestId('ProfileCard.lastname')).toHaveValue('admin');
+    });
+
+    test('Должна появиться ошибка', async () => {
+        componentRender(<EditableProfileCard id="1" />, options);
+        await userEvent.click(screen.getByTestId('EditableProfileCardHeader.EditButton'));
+
+        await userEvent.clear(screen.getByTestId('ProfileCard.firstname'));
+
+        await userEvent.click(screen.getByTestId('EditableProfileCardHeader.SaveButton'));
+
+        expect(screen.getByTestId('EditableProfileCard.Error.Paragraph')).toBeInTheDocument();
+    });
+
+    test('Если нет ошибок, то на сервер должен уйти PUT запрос', async () => {
+        const mockPutReq = jest.spyOn($api, 'put');
+        componentRender(<EditableProfileCard id="1" />, options);
+        await userEvent.click(screen.getByTestId('EditableProfileCardHeader.EditButton'));
+
+        await userEvent.clear(screen.getByTestId('ProfileCard.firstname'));
+
+        await userEvent.type(screen.getByTestId('ProfileCard.firstname'), 'user');
+
+        await userEvent.click(screen.getByTestId('EditableProfileCardHeader.SaveButton'));
+
+        expect(mockPutReq).toHaveBeenCalled();
     });
 });
