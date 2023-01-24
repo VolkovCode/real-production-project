a999079762d718d352018ca46ab08c7c9f79aeeb
Волков Алексей <wolf.alex.1997@mail.ru>
2023-01-24 14:18:39 +0300

    Обновлен собственный плагин для проверки путей

--- .eslintrc.js
+++ .eslintrc.js
@@ -72,7 +72,7 @@ module.exports = {
         'no-undef': 'off',
         'react/no-array-index-key': 'off',
         'arrow-body-style': 'off',
-        'volkovcode-plugin/path-checker': 'error',
+        'volkovcode-plugin/path-checker': ['error', {alias: '@'}],
     },
     globals: {
         __IS_DEV__: true,
